69e912e943f303badfa20ad76c301cd49d1d3e09
Anthony Serrano <light@anthonys-air.lan>
2021-07-14 18:45:14 -0400

    try

--- server.js
+++ server.js
@@ -2,14 +2,8 @@ var express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const { Client } = require("pg");
-<<<<<<< HEAD
-const pw = "Backend35";
-const connectionString =
-  "postgres://postgres:" + pw + "@localhost:5432/Bookstore8";
-=======
 const pw = "6100325";
 var indexRouter = require("./src/api/routes/index");
->>>>>>> 14f1cc7103abe5217d8a8bc6dfd828e997bb84b0
 
 app.use(express.json());
 app.use("/", indexRouter);
