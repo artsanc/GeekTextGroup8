69dce790aa31c00b3b58a80cd58fae48c71d6a64
ArielFSoler <87277772+ArielFSoler@users.noreply.github.com>
2021-07-18 21:39:46 -0400

    Create Comment/Rating
    Sends a new comment/rating together while user sends timestamp.

--- src/api/routes/shoppingCart.js
+++ src/api/routes/shoppingCart.js
@@ -1,62 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { Client } = require("pg");
-const pw = "6100325";
 
-const connectionString =
-  "postgres://postgres:" + pw + "@localhost:5432/Bookstore8";
-
-const client = new Client({
-  connectionString: connectionString
-});
-client.connect();
-
-router.get("/", async (req, res) => {
-  console.log("hola!");
-  res.send("in cart");
+router.get("/", async(req, res) => {
+    console.log("hola!");
+    res.send("in cart");
 });
 
-//Retrieve Endpoint to get the list of books in the shopping cart
-router.post("/getCart", async (req, res) => {
-  console.log(req.body);
-  client.query(
-    "SELECT isbn FROM shopping_cart where user_id = " + req.body.user_id,
-    function(err, result) {
-      if (err) {
-        console.log(err);
-        res.status(400).send(err);
-      }
-      res.status(200).send(result.rows);
-    }
-  );
-});
-
-//create a shopping cart and persist a book to the database
-router.post("/", async (req, res) => {
-  console.log(req.body.user_id);
-  console.log(req.body.isbn);
-  console.log(
-    "INSERT into shopping_cart (user_id, isbn) VALUES (" +
-      req.body.user_id +
-      ", " +
-      req.body.isbn +
-      ")"
-  );
-  client.query(
-    "INSERT into shopping_cart (user_id, isbn) VALUES (" +
-      req.body.user_id +
-      ", " +
-      req.body.isbn +
-      ")",
-    function(err, result) {
-      if (err) {
-        console.log(err);
-        res.status(400).send(err.detail);
-      } else {
-        res.status(200).send("Successfully Created A Shopping Cart");
-      }
-    }
-  );
-});
 
 module.exports = router;
