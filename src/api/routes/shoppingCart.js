aef6e74543396fa6dc589450545cf13fce05b442
Arturo Sanchez <arturosanchez@Arturos-MacBook-Pro.local>
2021-07-26 19:12:31 -0400

    delete endpoint

--- src/api/routes/shoppingCart.js
+++ src/api/routes/shoppingCart.js
@@ -4,38 +4,30 @@ const { Client } = require("pg");
 const pw = "6100325";
 
 const connectionString =
-    "postgres://postgres:" + pw + "@localhost:5432/Bookstore8";
+  "postgres://postgres:" + pw + "@localhost:5432/Bookstore8";
 
 const client = new Client({
-    connectionString: connectionString
+  connectionString: connectionString
 });
 client.connect().then(() => {
   console.log("i am connected to db");
 });
 
-function uuidv4() {
-    //bad uuid function need to switch uuid to include text
-    return new Date()
-        .getTime()
-        .toString()
-        .substring(0, 10);
-}
-
-function getTimestamp() {
-    return new Date();
-}
-
 //Retrieve Endpoint to get the list of books in the shopping cart
 router.post("/getCart", (req, res) => {
   console.log(req.body);
+  let isbn = [];
   client.query(
     "SELECT isbn FROM shopping_cart where user_id = " + req.body.user_id,
     function(err, result) {
       if (err) {
+        console.log("in here hi");
         console.log(err);
         res.status(400).send(err);
       } else {
-        res.status(200).send(result.rows);
+        console.log("in here");
+        isbn = result.rows;
+        console.log(isbn);
       }
     }
   );
@@ -81,6 +73,35 @@ router.post("/add", (req, res) => {
         res.status(400).send(err.detail);
       } else {
         res.status(200).send("Successfully Added a Book to Shopping Cart");
+      }
+    }
+  );
+});
+
+router.delete("/", (req, res) => {
+  console.log("the body,", req.body);
+  client.query(
+    "DELETE from shopping_cart_items where isbn = '" +
+      req.body.isbn +
+      "'" +
+      " and cart_id = '" +
+      req.body.cart_id +
+      "'",
+    function(err, result) {
+      if (err) {
+        console.log(err);
+        res.status(400).send(err.detail);
+      } else {
+        if (result.rowCount == 0) {
+          let temp =
+            "Book with isbn = " +
+            req.body.isbn +
+            " does not exist in cart " +
+            req.body.cart_id;
+          res.status(400).send(temp);
+        } else {
+          res.status(200).send("Deleted the book lets go!");
+        }
       }
     }
   );
